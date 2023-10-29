462cacce2eb2283cc713ac8e54956fedeaa1cf3e
Matthew Rheaume <mhr@lightspark.com>
2023-10-29 12:18:09 +0900

    [nvim] Lua completion from LSP.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,9 +1,5 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    -- build = "make install_jsregexp"
-  },
+  -- Completion
   {
     "hrsh7th/nvim-cmp",
     version = false,
@@ -24,20 +20,43 @@ return {
           end,
         },
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-        }, {
-          { name = "buffer" },
+          { name = "nvim_lsp", group_index = 1 },
+          { name = "luasnip", group_index = 1 },
+          { name = "path", group_index = 1 },
+          { name = "buffer", group_index = 2 },
         }),
       }
     end,
   },
+  -- Snippets
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    -- build = "make install_jsregexp"
+  },
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "neodev.nvim",
+      "mason.nvim",
+      "mason-lspconfig.nvim",
+    },
+    config = require('plugins.config.lspconfig'),
+  },
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    opts = {},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {},
+  },
+  -- NVIM development.
   {
     "folke/neodev.nvim",
     opts = {}
-  },
-  {
-    "neovim/nvim-lspconfig"
   },
 }
---
+++ nvim/lua/plugins/config/lspconfig.lua
@@ -0,0 +1,11 @@
+return function(_, _)
+  require("lspconfig").lua_ls.setup({
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" }
+        }
+      }
+    }
+  })
+end
