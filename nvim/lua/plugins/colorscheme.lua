e965ea0765c13eb6c700f5b5530838ed76d39372
Matthew Rheaume <mhr@lightspark.com>
2023-10-30 19:04:37 -0700

    [nvim] Switch to gruvbox-material.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true,
-    opts = {},
-  },
-  {
     "folke/tokyonight.nvim",
     priority = 1000,
-    config = true,
     opts = { style = "moon" },
   },
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = true,
+  }
 }
