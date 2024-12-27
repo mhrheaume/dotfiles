00d2a4aa20874a7e8b14ea9f2cc6ecbb0b67357e
Matthew Rheaume <mhr@lightspark.com>
2024-12-27 17:03:53 -0500

    [nvim] Disable keyword italics for tokyonight.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,12 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000,
-		opts = { style = "storm" },
+		opts = {
+			style = "storm",
+			styles = {
+				keywords = { italic = false },
+			},
+		},
 	},
 	{
 		"rebelot/kanagawa.nvim",
