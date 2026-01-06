d87f7699b9c38a4a9766e6a3bedaa21eb4edb2a4
Matthew Rheaume <mhr@lightspark.com>
2026-01-05 16:03:45 -0800

    [nvim] Simplify telescope file filtering.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -22,19 +22,7 @@ return {
 					"--files",
 					"--color",
 					"never",
-					"-uu",
 				}
-				for _, d in ipairs({
-					".git",
-					"node_modules",
-					"/target/",
-					"/build/",
-					"/.cache/",
-					"__pycache__",
-				}) do
-					table.insert(find_command, "-g")
-					table.insert(find_command, "!" .. d)
-				end
 
 				require("telescope.builtin").find_files({
 					find_command = find_command,
